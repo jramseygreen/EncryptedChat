74f79be36e02b66015686d082a02cd21d46c07b1
jramseygreen <44009946+jramseygreen@users.noreply.github.com>
2022-04-25 18:52:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,46 @@
 from client import Client
 from server import Server
+import time
 
 
 def show_menu():
+    print("Server active:", bool(server))
     for item in menu:
-        print(item)
+        print("[" + str(list(menu.keys()).index(item)) + "] " + item)
     print("")
 
+def add_user():
+    global server
+    server.add_account(input("Username: "), input("Password: "))
 
-menu = ["[1] Create chat server", "[2] Connect to chat server"]
+def del_user():
+    global server
+    server.del_account(input("Username: "))
+
+def create_chat_server():
+    global server
+    if server:
+        server.stop()
+    server = Server(input("Hostname or IP address: "), int(input("Port: ")))
+    server.start()
+    menu["Add user"] = add_user
+    menu["Remove user"] = del_user
+
+def connect_chat_server():
+    global client
+    host = input("Enter hostname or IP address to connect to: ")
+    port = int(input("Port: "))
+    client = Client(input("Username: "), input("Password: "), host, port)
+    client.start()
+    while client.is_running():
+        time.sleep(1)
+    client = None
+
+menu = {"Create chat server": create_chat_server,
+        "Connect to chat server": connect_chat_server}
 server = None
 client = None
 
 while True:
     show_menu()
-    choice = input("select -> ")
-    while not choice.isnumeric() and int(choice) <= len(menu) and int(choice) > 0:
-        print("Invalid selection!")
-        choice = input("select -> ")
-    if choice == "1":
-        server = Server(input("Enter host: "), int(input("Enter port: ")))
-        if len(menu) == 2:
-            menu.append("[" + str(len(menu) + 1) + "] Add User to server")
-            menu.append("[" + str(len(menu) + 1) + "] Remove User from server")
-            menu.append("[" + str(len(menu) + 1) + "] Start")
-    elif choice == "2":
-        host = input("Enter ip address or hostname to connect to: ")
-        client = Client(input("Enter username: "), input("Enter password: "), host)
-        if len(menu) == 2:
-            menu.append("[" + str(len(menu) + 1) + "] Start")
-    elif choice == "3":
-        server.add_account(input("username: "), input("password: "))
-    elif choice == "4":
-        server.del_account(input("Enter username of the account to delete: "))
-    elif choice == "5":
-        if server:
-            server.start()
-        if client:
-            client.start()
-        break
+    menu[list(menu.keys())[int(input("Selection -> "))]]()
